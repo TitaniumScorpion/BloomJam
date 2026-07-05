e32a1291002bc4d67a9cdf8ff46c23fe0f8de9f0
TitaniumScorpion <Uzayacik50@gmail.com>
2026-07-05 17:14:23 +0300

    SpawnerHitEffect

--- Assets/Scripts/DroneWeakPoint.cs
+++ Assets/Scripts/DroneWeakPoint.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class DroneWeakPoint : MonoBehaviour
 {
@@ -6,11 +7,70 @@ public class DroneWeakPoint : MonoBehaviour
     public SpawnerDrone parentDrone;
     public int damage = 1;
 
+    [Header("Hit Flash")]
+    public Renderer weakPointRenderer;
+    public Renderer weakPointRenderer2;
+    [ColorUsage(true, true)]
+    public Color flashEmissionColor = new Color(8f, 8f, 8f, 1f);
+    public float flashDuration = 0.1f;
+    public string emissionPropertyName = "_EmissionColor";
+
+    private Material instanceMaterial;
+    private Material instanceMaterial2;
+    private Color originalEmissionColor;
+    private Color originalEmissionColor2;
+    private int emissionColorID;
+    private Coroutine flashCoroutine;
+
+    private void Awake()
+    {
+        emissionColorID = Shader.PropertyToID(emissionPropertyName);
+
+        if (weakPointRenderer != null)
+        {
+            instanceMaterial = weakPointRenderer.material;
+            originalEmissionColor = instanceMaterial.GetColor(emissionColorID);
+            instanceMaterial.EnableKeyword("_EMISSION");
+        }
+
+        if (weakPointRenderer2 != null)
+        {
+            instanceMaterial2 = weakPointRenderer2.material;
+            originalEmissionColor2 = instanceMaterial2.GetColor(emissionColorID);
+            instanceMaterial2.EnableKeyword("_EMISSION");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (instanceMaterial != null) Destroy(instanceMaterial);
+        if (instanceMaterial2 != null) Destroy(instanceMaterial2);
+    }
+
     public void TakeDamage(int incomingDamage)
     {
         if (parentDrone != null)
             parentDrone.TakeDamage(damage);
-        else
-            Debug.LogWarning("DroneWeakPoint hit but parentDrone is not assigned!", this);
+
+        if (gameObject.activeInHierarchy)
+        {
+            if (flashCoroutine != null) StopCoroutine(flashCoroutine);
+            flashCoroutine = StartCoroutine(FlashRoutine());
+        }
+    }
+
+    private IEnumerator FlashRoutine()
+    {
+        if (instanceMaterial != null)
+            instanceMaterial.SetColor(emissionColorID, flashEmissionColor);
+        if (instanceMaterial2 != null)
+            instanceMaterial2.SetColor(emissionColorID, flashEmissionColor);
+
+        yield return new WaitForSeconds(flashDuration);
+
+        if (instanceMaterial != null)
+            instanceMaterial.SetColor(emissionColorID, originalEmissionColor);
+        if (instanceMaterial2 != null)
+            instanceMaterial2.SetColor(emissionColorID, originalEmissionColor2);
     }
 }
